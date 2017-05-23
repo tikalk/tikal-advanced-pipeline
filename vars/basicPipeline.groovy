724d36a39b5f21a92662acfa45a84d024d26ef66
Yoram Michaeli <yorammi@tikalk.com>
2017-05-23 12:23:03 +0300

    Update basicPipeline.groovy

--- vars/basicPipeline.groovy
+++ vars/basicPipeline.groovy
@@ -2,10 +2,10 @@
 def call(body) 
 {
   // evaluate the body block, and collect configuration into the object
-  def config = [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = config
-  body()
+  // def config = [:]
+  // body.resolveStrategy = Closure.DELEGATE_FIRST
+  // body.delegate = config
+  // body()
   
   node {
     stage ('First Stage'){
