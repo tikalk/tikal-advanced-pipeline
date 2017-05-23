e4e6f04dc5dc1dc621955619f98e15cc4fffa20c
Yoram Michaeli <yorammi@tikalk.com>
2017-05-23 09:03:37 +0300

    Create basicPipeline.groovy

---
+++ vars/basicPipeline.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/groovy
+def call(body) 
+{
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+  
+  node {
+    stage ('First Stage'){
+      echo "This is first stage"
+    }
+    stage('Second Stage'){
+      echo "This is second stage"
+    }
+  }
+}
