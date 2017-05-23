80e18c656c918cc92aaafba48e0ec24f70647241
Yoram Michaeli <yorammi@tikalk.com>
2017-05-23 13:21:13 +0300

    Update basicPipeline.groovy

--- vars/basicPipeline.groovy
+++ vars/basicPipeline.groovy
@@ -1,18 +1,35 @@
 #!/usr/bin/groovy
+
 def call(body) 
 {
   // evaluate the body block, and collect configuration into the object
-  // def config = [:]
-  // body.resolveStrategy = Closure.DELEGATE_FIRST
-  // body.delegate = config
-  // body()
+  //def config = [:]
+  //body.resolveStrategy = Closure.DELEGATE_FIRST
+  //body.delegate = config
+  mainFlow()
   
-  node {
-    stage ('First Stage'){
-      echo "This is first stage"
+}
+
+def mainFlow()
+{
+    gitUpdate("git@...")
+    mavenBuild()
+}
+
+def gitUpdate(String gitURL, String gitBranch = "master", String phaseTitle = "GIT checkout")
+{
+    stage( phaseTitle ) {
+        echo "GIT URL ==> " + gitURL;
+        echo "GIT BRANCH ==> " + gitBranch;
     }
-    stage('Second Stage'){
-      echo "This is second stage"
+    
+}
+
+def mavenBuild(String mavenPomPath = "pom.xml", String mavenGoals = "clean install", String phaseTitle = "Maven build")
+{
+    stage( phaseTitle ) {
+        echo "Maven POM path ==> " + mavenPomPath;
+        echo "MAVEN goals ==> " + mavenGoals;
     }
-  }
+    
 }
