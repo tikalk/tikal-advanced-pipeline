80ffd0a50ee337bd9f48260e7f8f83636f1cc5c7
doronshai <doronshai@gmail.com>
2017-06-18 14:19:32 +0300

    Update notifyBuild.groovy

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -2,35 +2,21 @@
 
 import settings_mail
 
-def call(String buildStatus = 'STARTED', String mailContentFile) {
+def call(settings_mail set) {
     // build status of null means successful
-    buildStatus = buildStatus ?: 'SUCCESS'
+    buildStatus = set.buildStatus ?: 'SUCCESS'
 
     // Default values
     //def TEST = '\${CHANGES, showPaths=true, format="%a: %r %p \n--\"%m\"", pathFormat="\n\t- %p"}'
-    def colorName = 'RED'
-    def colorCode = '#FF0000'
-    def mailSubject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-    def mailContent = readFile mailContentFile
+    def mailSubject = "${set.buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
+    def mailContent = readFile set.mailContentFile
                                // 'popcorn/pipelines/CI-master/summary.html'
-
-    // Override default values based on build status
-    if (buildStatus == 'STARTED') {
-        color = 'YELLOW'
-        colorCode = '#FFFF00'
-    } else if (buildStatus == 'SUCCESS') {
-        color = 'GREEN'
-        colorCode = '#00FF00'
-    } else {
-        color = 'RED'
-        colorCode = '#FF0000'
-    }
 
     // Requires "user build vars plugin"
     wrap([$class: 'BuildUser']) {
         echo "Send SUMMARY HTML Mail ..."
         emailext(to: "${env.DEFAULT_RECIPIENTS}", replyTo: "dorons@tikalk.com",
-                mimeType: 'text/html', subject: mailSubject, body: mailContent,
+                mimeType: 'text/html', subject: set.mailSubject, body: set.mailContent,
                 recipientProviders: [[$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider']]);
     }
     emailext attachmentsPattern: '**/test.html', body: readFile 'test.html', subject: summary, to: "${env.DEFAULT_RECIPIENTS}", mimeType: 'text/html', recipientProviders: [[$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider']]
