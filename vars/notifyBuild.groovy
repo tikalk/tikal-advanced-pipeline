78450a17b9b9d90e7c63678ab32398c5b023aaf7
doronshai <doronshai@gmail.com>
2017-06-18 13:52:21 +0300

    Update notifyBuild.groovy

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/groovy
 
+@NonCPS
 def call(String buildStatus = 'STARTED', String mailContentFile) {
     // build status of null means successful
     buildStatus = buildStatus ?: 'SUCCESS'
