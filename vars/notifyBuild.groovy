1792bb5a156f7bef2d1bd3e2f3d59b5363d4e7c5
doronshai <doronshai@gmail.com>
2017-06-18 15:15:03 +0300

    Update notifyBuild.groovy

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -1,22 +1,21 @@
 #!/usr/bin/groovy
 
-import settings_mail
-
-def call(settings_mail set) {
+@NonCPS
+def call(String buildStatus = 'STARTED', String mailContentFile) {
     // build status of null means successful
-    buildStatus = set.buildStatus ?: 'SUCCESS'
+    buildStatus = buildStatus ?: 'SUCCESS'
 
     // Default values
     //def TEST = '\${CHANGES, showPaths=true, format="%a: %r %p \n--\"%m\"", pathFormat="\n\t- %p"}'
-    def mailSubject = "${set.buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-    def mailContent = readFile set.mailContentFile
+    def mailSubject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
+    def mailContent = readFile mailContentFile
                                // 'popcorn/pipelines/CI-master/summary.html'
 
     // Requires "user build vars plugin"
     wrap([$class: 'BuildUser']) {
         echo "Send SUMMARY HTML Mail ..."
         emailext(to: "${env.DEFAULT_RECIPIENTS}", replyTo: "dorons@tikalk.com",
-                mimeType: 'text/html', subject: set.mailSubject, body: set.mailContent,
+                mimeType: 'text/html', subject: mailSubject, body: mailContent,
                 recipientProviders: [[$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider']]);
     }
     //emailext attachmentsPattern: '**/test.html', body: readFile 'test.html', subject: summary, to: "${env.DEFAULT_RECIPIENTS}", mimeType: 'text/html', recipientProviders: [[$class: 'DevelopersRecipientProvider'],[$class: 'CulpritsRecipientProvider']]
