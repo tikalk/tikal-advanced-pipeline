2ec6e6b59ba21de2603bfd6be1eeb95c27f3bf08
doronshai <doronshai@gmail.com>
2017-06-20 11:53:17 +0300

    Update TAP_email.groovy

--- vars/TAP_email.groovy
+++ vars/TAP_email.groovy
@@ -5,26 +5,27 @@ def call(String buildStatus, String mailContentFile) {
     // build status of null means successful
     buildStatus = buildStatus ?: 'SUCCESS'
 
-    def out
-    def config = new HashMap()
-    def bindings = getBinding()
-    config.putAll(bindings.getVariables())
-    out = config['out']
+    //def out
+    //def config = new HashMap()
+    //def bindings = getBinding()
+    //config.putAll(bindings.getVariables())
+    //out = config['out']
     
-    out.println "================== Printed do Jenkins console 1 ======================"
-    out.println "================== buildStatus    ${buildStatus} ======================"
-    out.println "================== JOB_NAME       ${env.JOB_NAME} ======================"
-    out.println "================== BUILD_NUMBER   ${env.BUILD_NUMBER} ======================"
-    out.println "================== BUILD_URL      ${env.BUILD_URL} ======================"
+    
+    manager.listener.logger.println "================== Printed do Jenkins console 1 ======================"
+    manager.listener.logger.println "================== buildStatus    ${buildStatus} ======================"
+    manager.listener.logger.println "================== JOB_NAME       ${env.JOB_NAME} ======================"
+    manager.listener.logger.println "================== BUILD_NUMBER   ${env.BUILD_NUMBER} ======================"
+    manager.listener.logger.println "================== BUILD_URL      ${env.BUILD_URL} ======================"
 
     def mailSubject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})"
-    out.println "================== mailSubject      "+ mailSubject + " ======================"
+    manager.listener.logger.println "================== mailSubject      "+ mailSubject + " ======================"
 
-    out.println "================== Printed do Jenkins console 2 ====================="
-    out.println "================== Printed do Jenkins console 2 ====================="
-    out.println "================== Printed do Jenkins console 2 ====================="
+    manager.listener.logger.println "================== Printed do Jenkins console 2 ====================="
+    manager.listener.logger.println "================== Printed do Jenkins console 2 ====================="
+    manager.listener.logger.println "================== Printed do Jenkins console 2 ====================="
     
-    out.println "================== mailContentFile    " + mailContentFile + " ====================="
+    manager.listener.logger.println "================== mailContentFile    " + mailContentFile + " ====================="
     
     //String 
     String mailContent = readFile mailContentFile
