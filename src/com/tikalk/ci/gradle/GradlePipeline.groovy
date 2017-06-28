f553110df3ed133c8cb70151c0ee81a57f6343d2
Itai <itai@tikalk.com>
2017-06-28 15:50:37 +0300

    Add Import

---
+++ src/com/tikalk/ci/gradle/GradlePipeline.groovy
@@ -0,0 +1,54 @@
+package com.tikalk.ci.gradle;
+
+import com.tikalk.ci.BasePipeline
+
+class GradlePipeline extends BasePipeline {
+    boolean debugMode
+    int waitForInputTimeout
+
+
+    GradlePipeline(script) {
+        super(script)
+    }
+
+
+    @Override
+    void populateBuildDisplayName() {
+        script.currentBuild.displayName = "${script.currentBuild.displayName} ${retrieveCurrentBuildUser()}"
+    }
+
+    void retrieveCurrentBuildUser() {
+        script.wrap([$class: 'BuildUser']) {
+            script.sh returnStdout: true, script: 'echo ${BUILD_USER}'
+        }
+    }
+
+    @Override
+    void build() {
+        logger.info "Implements gradle build here"
+    }
+
+
+    void waitForInput() {
+        script.timeout(time: waitForInputTimeout, unit: 'MINUTES') {
+            if (debugMode) {
+                script.input 'Continue with cleanup?'
+            }
+        }
+    }
+
+
+
+    void compile(Map m) {
+
+    }
+
+    void buildDocker() {
+
+    }
+
+
+
+
+
+}
