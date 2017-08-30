bb2ab0c3b995ac94d1c593e2f8ec9a7726f8d909
Itai <itai@tikalk.com>
2017-08-30 15:36:22 +0300

    Add CredentialsId and url

--- src/com/tikalk/ci/gradle/GradlePipeline.groovy
+++ src/com/tikalk/ci/gradle/GradlePipeline.groovy
@@ -27,7 +27,7 @@ class GradlePipeline extends BasePipeline {
     @Override
     void initParams()
     {
-        super.initParams()
+        this.&initParams()
         buildTarget = script.params.buildTarget //script.params.// Implement to set params that are not able to set in constructor (due to @NonCPS etc)
 
     }
